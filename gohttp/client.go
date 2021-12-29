deec820c860179168ec5625328618923d1957501
Saad Guessous <guessous_saad@hotmail.com>
2021-12-29 22:19:23 +0200

    Add Http interface

---
+++ gohttp/client.go
@@ -0,0 +1,26 @@
+package gohttp
+
+type httpClient struct{}
+
+func New() HttpClient {
+	client := &httpClient{}
+	return client
+}
+
+type HttpClient interface {
+	Get()
+	Post()
+	Put()
+	Patch()
+	Delete()
+}
+
+func (c *httpClient) Get() {}
+
+func (c *httpClient) Post() {}
+
+func (c *httpClient) Patch() {}
+
+func (c *httpClient) Put() {}
+
+func (c *httpClient) Delete() {}
